aef97e68e5110f9f3a8954921c46706d331c6fb0
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-08 20:03:05 -0600

    Added postgres dbs and fixed db connection strings.

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        'postgresql://flaskydevuser:flaskydevpw@localhost/flaskydev'
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
+        'postgresql://flaskytestuser:flaskytestpw@localhost/flaskytest'
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
