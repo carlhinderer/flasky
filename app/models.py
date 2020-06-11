7823313a50c44a62e3957c581615a12d91851b20
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-10 23:33:24 -0600

    Fixed User model and imported LoginManager.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
+from . import login_manager
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -35,13 +36,6 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
-
-
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
