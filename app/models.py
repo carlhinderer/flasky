c35e93f346182201f49d2d2f8a4855d30c29cb0f
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-13 09:31:00 -0600

    Added methods to manage permissions to the Role model.

--- app/models.py
+++ app/models.py
@@ -32,6 +32,20 @@ class Role(db.Model):
         if self.permissions is None:
             self.permissions = 0
 
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permission(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
