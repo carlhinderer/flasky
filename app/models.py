190ae31a608c1e8cd73fc8f393207ba54a0548de
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-12 23:31:22 -0600

    Added default and permissions columns to roles table.

--- app/models.py
+++ app/models.py
@@ -15,10 +15,14 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
 
 
 class User(UserMixin, db.Model):
