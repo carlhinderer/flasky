4292ddf7d4103c21e9293222a2ba8bc4317029ad
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-12 20:23:41 -0600

    Now sending confirmation email when new user is registered.

--- app/auth/views.py
+++ app/auth/views.py
@@ -38,6 +38,9 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                   'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
