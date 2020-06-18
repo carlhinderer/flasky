07e617d342a58d068e627e9526a7547f213de5c4
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-18 15:54:07 -0600

    Added new form and updated index() view method for authoring blog posts.

--- app/main/views.py
+++ app/main/views.py
@@ -3,30 +3,22 @@ from flask import flash, redirect, render_template, session, url_for
 from flask_login import current_user, login_required
 
 from . import main
-from .forms import EditProfileAdminForm, EditProfileForm, NameForm
+from .forms import EditProfileAdminForm, EditProfileForm, NameForm, PostForm
 from .. import db
 from ..decorators import admin_required
-from ..models import Role, User
+from ..models import Permission, Post, Role, User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
+    form = PostForm()
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/user/<username>')
