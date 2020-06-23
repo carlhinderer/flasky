e75fd03a05afec9138639678ce4e7cd3e104f3df
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-23 13:33:04 -0600

    Added pagination of posts to index view method.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import flash, redirect, render_template, session, url_for
+from flask import current_app, flash, redirect, render_template, request, session, url_for
 from flask_login import current_user, login_required
 
 from . import main
@@ -17,8 +17,13 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, 
+        pagination=pagination)
 
 
 @main.route('/user/<username>')
--- config.py
+++ config.py
@@ -14,6 +14,8 @@ class Config:
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    FLASKY_POSTS_PER_PAGE = 20
+
     @staticmethod
     def init_app(app):
         pass
