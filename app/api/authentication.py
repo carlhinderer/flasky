047c77bbe55f79b765f5a36a0e5fd92078e27257
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-26 10:23:14 -0600

    Finished adding handlers for Basic Authentication.

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,5 +1,8 @@
 from flask_httpauth import HTTPBasicAuth
+from .errors import forbidden, unauthorized
+
 auth = HTTPBasicAuth()
+
 
 @auth.verify_password
 def verify_password(email, password):
@@ -10,3 +13,16 @@ def verify_password(email, password):
         return False
     g.current_user = user
     return user.verify_password(password)
+
+
+@auth.error_handler
+def auth_error():
+    return unauthorized('Invalid credentials')
+
+
+@api.before_request
+@auth.login_required
+def before_request():
+    if not g.current_user.is_anonymous and \
+            not g.current_user.confirmed:
+        return forbidden('Unconfirmed account')
