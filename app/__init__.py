3b3e51b1bba8c11a33859037c9c2dd687ac4a8f9
Carl Hinderer <carl.hinderer4@gmail.com>
2020-08-04 21:44:15 -0600

    Added PageDown packages.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
@@ -10,6 +11,7 @@ from config import config
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
+pagedown = PageDown()
 db = SQLAlchemy()
 
 login_manager = LoginManager()
@@ -23,6 +25,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
+    pagedown.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
@@ -32,7 +35,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint 
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-    from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+    # from .api import api as api_blueprint
+    # app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
