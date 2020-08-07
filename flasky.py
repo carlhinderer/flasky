5f6f79240e853d7dac157eb06330ebf826a2a532
Carl Hinderer <carl.hinderer4@gmail.com>
2020-08-07 17:03:15 -0600

    Added coverage to application.

--- flasky.py
+++ flasky.py
@@ -1,4 +1,4 @@
-import os
+import os, sys, click
 from app import create_app, db
 from app.models import Follow, Permission, Post, Role, User
 from flask_migrate import Migrate
@@ -6,14 +6,34 @@ from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, Follow=Follow, Permission=Permission, Post=Post, 
         Role=Role, User=User)
 
 @app.cli.command()
-def test():
-    """Run the unit tests."""
+@click.option('--coverage/--no-coverage', default=False,
+              help='Run tests under code coverage.')
+def test(coverage):
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
